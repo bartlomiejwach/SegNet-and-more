209ffff69adf150a96261031b654f718f8dd1e6f
bartlomiejwach <01131205@pw.edu.pl>
2021-04-07 21:47:03 +0200

    AlexNet Working

--- library.py
+++ library.py
@@ -5,7 +5,6 @@ from keras.layers import Dense, Conv2D, MaxPool2D , Flatten, Dropout, BatchNorma
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
-import pickle
 import os
 
 #GPU Init
@@ -17,43 +16,8 @@ if gpus:
   except RuntimeError as e:
     print(e)
 
-#DataSet
-(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
-CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-validation_images, validation_labels = train_images[:5000], train_labels[:5000]
-train_images, train_labels = train_images[5000:], train_labels[5000:]
-
-train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
-
-def process_images(image, label):
-    # Normalize images to have a mean of 0 and standard deviation of 1
-    image = tf.image.per_image_standardization(image)
-    # Resize images from 32x32 to 277x277
-    image = tf.image.resize(image, (227,227))
-    return image, label
-
-train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
-test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
-validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
-
-train_ds = (train_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-test_ds = (test_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-validation_ds = (validation_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-
 #AlexNet Model
-def AlexNet():
+def AlexNet(train_data, epochs, validation_data, validation_freq):
 
     model = Sequential()
 
@@ -104,10 +68,7 @@ def AlexNet():
               optimizer='adam',
               metrics=['accuracy'])
 
-    #model.summary()
-
-    model.fit(train_ds,epochs=1,validation_data=validation_ds,validation_freq=1)
+    #model.fit(train_ds, epochs=1, validation_data=validation_ds, validation_freq=1)
+    model.fit(train_data, epochs=epochs, validation_data=validation_data, validation_freq=validation_freq)
 
     model.save('new.model')
-
-AlexNet()
