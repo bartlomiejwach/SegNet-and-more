e54cf51a1081abb8ade6b3ba0d28389eaa021043
bartlomiejwach <01131205@pw.edu.pl>
2021-04-07 22:12:44 +0200

    AlexNet Working Good

--- library.py
+++ library.py
@@ -1,11 +1,7 @@
 #Libraries used
 from keras.models import Sequential
-from tensorflow.keras.datasets import cifar10
 from keras.layers import Dense, Conv2D, MaxPool2D , Flatten, Dropout, BatchNormalization, Activation
-import matplotlib.pyplot as plt
 import tensorflow as tf
-import numpy as np
-import os
 
 #GPU Init
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -16,59 +12,72 @@ if gpus:
   except RuntimeError as e:
     print(e)
 
+#Models
+#Models will be saved as Model_Name.model
+
 #AlexNet Model
-def AlexNet(train_data, epochs, validation_data, validation_freq):
+def AlexNet(train_data, validation_data, epochs, validation_freq):
 
-    model = Sequential()
+  def process_images(image, label):
+    #Resize
+    image = tf.image.per_image_standardization(image)
+    #Resize
+    image = tf.image.resize(image, (227,227))
+    return image, label
 
-    model.add(Conv2D(filters=96, input_shape=(227,227,3), kernel_size=(11,11), strides=(4,4), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
+  train_data_size = tf.data.experimental.cardinality(train_data).numpy()
+  validation_data_size = tf.data.experimental.cardinality(validation_data).numpy()
 
-    model.add(Conv2D(filters=256, kernel_size=(5, 5), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
+  train_data = (train_data.map(process_images).shuffle(buffer_size=train_data_size).batch(batch_size=32, drop_remainder=True))
+  validation_data = (validation_data.map(process_images).shuffle(buffer_size=train_data_size).batch(batch_size=32, drop_remainder=True))
 
-    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
+  model = Sequential()
 
-    model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
+  model.add(Conv2D(filters=96, input_shape=(227,227,3), kernel_size=(11,11), strides=(4,4), padding='same'))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
 
-    model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
+  model.add(Conv2D(filters=256, kernel_size=(5, 5), strides=(1,1), padding='same'))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
 
-    model.add(Flatten())
-    model.add(Dense(4096, input_shape=(32,32,3,)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.4))
+  model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
 
-    model.add(Dense(4096))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.4))
+  model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
 
-    model.add(Dense(1000))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.4))
+  model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same'))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
 
-    model.add(Dense(10))
-    model.add(BatchNormalization())
-    model.add(Activation('softmax'))
+  model.add(Flatten())
+  model.add(Dense(4096, input_shape=(32,32,3,)))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(Dropout(0.4))
 
-    model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+  model.add(Dense(4096))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(Dropout(0.4))
 
-    #model.fit(train_ds, epochs=1, validation_data=validation_ds, validation_freq=1)
-    model.fit(train_data, epochs=epochs, validation_data=validation_data, validation_freq=validation_freq)
+  model.add(Dense(1000))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(Dropout(0.4))
 
-    model.save('new.model')
+  model.add(Dense(10))
+  model.add(BatchNormalization())
+  model.add(Activation('softmax'))
+
+  model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+  model.fit(train_data, epochs=epochs, validation_data=validation_data, validation_freq=validation_freq)
+
+  model.save('AlexNet.model')
--- train.py
+++ train.py
@@ -21,32 +21,7 @@ CLASS_NAMES= ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'h
 validation_images, validation_labels = train_images[:5000], train_labels[:5000]
 train_images, train_labels = train_images[5000:], train_labels[5000:]
 
-train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-validation_ds = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
+train_data = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+validation_data = tf.data.Dataset.from_tensor_slices((validation_images, validation_labels))
 
-def process_images(image, label):
-    # Normalize images to have a mean of 0 and standard deviation of 1
-    image = tf.image.per_image_standardization(image)
-    # Resize images from 32x32 to 277x277
-    image = tf.image.resize(image, (227,227))
-    return image, label
-
-train_ds_size = tf.data.experimental.cardinality(train_ds).numpy()
-test_ds_size = tf.data.experimental.cardinality(test_ds).numpy()
-validation_ds_size = tf.data.experimental.cardinality(validation_ds).numpy()
-
-train_ds = (train_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-test_ds = (test_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-validation_ds = (validation_ds
-                  .map(process_images)
-                  .shuffle(buffer_size=train_ds_size)
-                  .batch(batch_size=32, drop_remainder=True))
-
-AlexNet(train_ds, 1, validation_ds, 1)
+AlexNet(train_data, validation_data, 1, 1)
